f243904197221f7a0e2d6f8ae2ef5cd49eb3d430
Tiago Silva <tiago.silva.1996@hotmail.com>
2018-02-23 17:24:39 +0000

    First commit , implemented character moviment, interaction with items and inventory

---
+++ Assets/ItemPickUp.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ItemPickUp : Interactable
+{
+
+    public Item item;
+    public override void Interact()
+    {
+        base.Interact();
+
+        PickUp();
+    }
+
+    void PickUp()
+    {
+        Debug.Log(item.name);
+        bool wasPickedUp = Inventory.instance.Add(item);
+        if (wasPickedUp)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+    public float radius = 3f;
+    public Transform interactionTransform;
+
+    bool isFocus = false;
+    Transform player;
+    bool hasInterected = false;
+    public virtual void Interact()
+    {
+        // To be Overwritten by object or npc
+    }
+    void Update()
+    {
+        if (isFocus && !hasInterected)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if(distance <= radius)
+            {
+                Interact();
+                hasInterected = true;
+            }
+        }
+    }
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInterected = false;
+    }
+
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInterected = false;
+    }
+    void OnDrawGizmosSelected()
+    {
+        if (interactionTransform == null)
+            interactionTransform = transform;
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position,radius);
+    }
+}
