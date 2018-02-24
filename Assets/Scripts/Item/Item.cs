3030b4517d205e14ea129fe7d8d10c1f34762d33
Tiago Silva <tiago.silva.1996@hotmail.com>
2018-02-24 21:51:06 +0000

    Items, inventory, and equipments

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -6,4 +6,13 @@ public class Item : ScriptableObject {
    new public string name = "New Item";
     public Sprite icon = null;
     public bool isDefaultItem = false;
+    public virtual void Use()
+    {
+
+    }
+
+    public  void RemoveFromInventory()
+    {
+        Inventory.instance.Remove(this);
+    }
 }
